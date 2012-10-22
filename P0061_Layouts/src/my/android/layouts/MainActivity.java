084006bb3884231c0ba0b7468b8bc7fbbbafe083
Victor <v.vovchenko91@gmail.com>
2012-10-22 22:35:40 +0300

    Lesson 6, added table layout

--- P0061_Layouts/src/my/android/layouts/MainActivity.java
+++ P0061_Layouts/src/my/android/layouts/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.tlayout);
     }
 
     @Override
