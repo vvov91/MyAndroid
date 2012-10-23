0807c63e827891a1d10096975fbf0416b3cb1df0
Victor <v.vovchenko91@gmail.com>
2012-10-23 19:50:25 +0300

    Lesson 14. Menu through xml

--- P0141_AdvancedMenu/gen/my/android/advancedmenu/R.java
+++ P0141_AdvancedMenu/gen/my/android/advancedmenu/R.java
@@ -16,6 +16,13 @@ public final class R {
     }
     public static final class id {
         public static final int chbExtMenu=0x7f070001;
+        public static final int group1=0x7f070006;
+        public static final int menu_add=0x7f070003;
+        public static final int menu_copy=0x7f070007;
+        public static final int menu_delete=0x7f070005;
+        public static final int menu_edit=0x7f070004;
+        public static final int menu_exit=0x7f070009;
+        public static final int menu_paste=0x7f070008;
         public static final int menu_settings=0x7f070002;
         public static final int textView=0x7f070000;
     }
@@ -24,6 +31,7 @@ public final class R {
     }
     public static final class menu {
         public static final int main=0x7f060000;
+        public static final int mymenu=0x7f060001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
--- P0141_AdvancedMenu/src/my/android/advancedmenu/MainActivity.java
+++ P0141_AdvancedMenu/src/my/android/advancedmenu/MainActivity.java
@@ -3,7 +3,6 @@ package my.android.advancedmenu;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
@@ -22,31 +21,13 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-    	menu.add(0, 1, 0, "Add");
-        menu.add(0, 2, 0, "Edit");
-        menu.add(0, 3, 3, "Delete");
-        menu.add(1, 4, 1, "Copy");
-        menu.add(1, 5, 2, "Paste");
-        menu.add(1, 6, 4, "Exit");
-        return true;
+    	 getMenuInflater().inflate(R.menu.mymenu, menu);
+    	 return super.onCreateOptionsMenu(menu);
     }
     
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
-      menu.setGroupVisible(1, chb.isChecked());
+      menu.setGroupVisible(R.id.group1, chb.isChecked());
       return super.onPrepareOptionsMenu(menu);
-    }
-    
-    public boolean onOptionsItemSelected(MenuItem item) {
-      StringBuilder sb = new StringBuilder();
-
-      sb.append("Item Menu");
-      sb.append("\r\n groupId: " + String.valueOf(item.getGroupId()));
-      sb.append("\r\n itemId: " + String.valueOf(item.getItemId()));
-      sb.append("\r\n order: " + String.valueOf(item.getOrder()));
-      sb.append("\r\n title: " + item.getTitle());
-      tv.setText(sb.toString());
-      
-      return super.onOptionsItemSelected(item);
     }
 }
